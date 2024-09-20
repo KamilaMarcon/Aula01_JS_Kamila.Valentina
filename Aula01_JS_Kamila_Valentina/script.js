b5f8bc38f193463e7ac7a50f682446e1299454e3
KamilaMarcon <kamila.marcon@escola.pr.gov.br>
2024-09-20 08:09:06 -0300

    Add files via upload

--- Aula01_JS_Kamila_Valentina/script.js
+++ Aula01_JS_Kamila_Valentina/script.js
@@ -5,3 +5,9 @@ var nome = prompt("qual é seu nome?");
 var sobrenome = prompt("Qual seu sobrenome?");
 
     console.log ("Olá " + nome + " " + sobrenome);
+    
+    //saída no DOM
+    document.write("<h1>Bem vindo "+nome+" "+sobrenome+"</h1>");
+
+    document.write('<img src="https://sujeitoprogramador.com/steve.png" alt="foto do steve" />')
+    
